d1f2f7b6f3caed58fcc95d0634067565aef10c87
stringcode <michael@stringcode.co.uk>
2017-07-26 09:37:09 +0100

    Initial Commit

---
+++ AlertViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertViewController
+//
+//  Created by Michael Inger on 26/07/2017.
+//  Copyright © 2017 stringCode ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
