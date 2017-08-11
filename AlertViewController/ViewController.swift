5820c3005d7c7b28607988e70c5ed98376ce623d
stringcode <michael@stringcode.co.uk>
2017-08-11 19:46:16 +0100

    minor clean up

--- AlertViewController/ViewController.swift
+++ AlertViewController/ViewController.swift
@@ -10,29 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
     @IBAction func doStuffAction(_ sender: Any) {
-        let alert = AlertController(title: "Tesing", message: "Wubba lubba dub dub, really long text. That is what I need to test in here", preferredStyle: .alert)
+        // Create AlertController
+        let alert = AlertController(title: "Tesing", message: "Wubba lubba dub dub", preferredStyle: .alert)
         alert.setTitleImage(UIImage(named: "alert"))
+        // Add actions
         let action = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        //action.setValue(UIImage(named: "close"), forKey: "image")
         action.actionImage = UIImage(named: "close")
-        alert.addAction(action)
         alert.addAction(UIAlertAction(title: "Default", style: .default, handler: nil))
         alert.addAction(UIAlertAction(title: "Destroy", style: .destructive, handler: nil))
-
-        
-//        let imageView = UIImageView(image: UIImage(named: "alert"))
-//        alert.view.addSubview(imageView)
-//        imageView.center.x = alert.view.bounds.width / 2
-    
-        
+        alert.addAction(action)
         present(alert, animated: true, completion: nil)
-
     }
 
 }
